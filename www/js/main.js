d9e0219d005b9dc0257815482ce72d1b53e6c8b0
Aditya Mishra <75035219+DEVLOPRR@users.noreply.github.com>
2022-07-08 12:43:05 +0530

    Disable Drag & Drop

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,21 @@
+function disableDragNDrop(element) {
+    // Disable Drag And Drop
+    let evtNames = ['dragenter', 'dragover', 'dragleave', 'drop'];
+    for (let i = 0; i < evtNames.length; i++) {
+        element.addEventListener(evtNames[i], function(e) {
+            e.preventDefault()
+            e.stopPropagation()
+        }, false);
+    }
+}
+
 function onWindowClose() {
     Neutralino.app.exit();
 }
 
 Neutralino.init();
 Neutralino.events.on("windowClose", onWindowClose);
+
+window.onload = function() {
+    disableDragNDrop(document.body);
+}
